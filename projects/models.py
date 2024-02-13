10fd7aee736026597094962dfb2b6158609ec53e
Tomasz Wewior <kontowowag@gmail.com>
2024-02-13 22:28:20 +0100

    Upload main project

---
+++ projects/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.CharField(max_length=250)
+    technology = models.CharField(max_length=30)
+    image = models.FileField(upload_to="project_images/", blank=True)
+    githublink = models.CharField(max_length=60)
