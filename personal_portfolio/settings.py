019eb9482074b366fe9d59d9b9bbf70895a694a7
Tomasz Wewior <kontowowag@gmail.com>
2024-02-16 21:45:13 +0100

    Adding key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@cm-c2uz#$d2494$r=lqr7lefzxozrf7vyg!)&k1$4kh4j7wy%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
