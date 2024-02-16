b2323977c5b51c789387eb25afa9108bcbb773c8
Tomasz Wewior <kontowowag@gmail.com>
2024-02-16 21:55:07 +0100

    After loading pythonanywhere

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-@cm-c2uz#$d2494$r=lqr7lefzxozrf7vyg!)&k1$4kh4j7wy%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
